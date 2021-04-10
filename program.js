263d2128a02147d92bfa2d132c3e4416aeea7ea3
antekrtalic <antekrtalic2@gmail.com>
2021-04-10 21:37:30 +0200

    Adding new variables for winning counting

--- program.js
+++ program.js
@@ -17,16 +17,22 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
+    let player_count = 0;
+    let computer_count = 0;
     let msg = "";
     
     if (player === "paper"){
+
         if (computer === "rock"){
             msg = "You Won!Paper beats Rock.";
+            player_count += 1;
         }else if (computer === "paper"){
             msg = "It's Draw!Paper can't beat Paper."
         }else{
             msg = "You Lose!Scissors beats Paper.";
+            computer_count += 1;
         }
+
     }else if(player === "rock"){
 
         if (computer === "rock"){
@@ -34,16 +40,20 @@ function playRound(playerSelection, computerSelection){
             
         }else if (computer === "paper"){
             msg = "You Lose!Paper beats Rock."
+            computer_count += 1;
         }else{
             msg = "You Won!Rock beats Scissors.";
+            player_count += 1;
         }
 
     }else if(player === "scissors"){
 
         if(computer === "rock"){
             msg = "You Lost! Rock beats Scissors!";
+            computer_count += 1;
         }else if(computer === "paper"){
             msg = "You Won! Scissors beats Paper.";
+            player_count += 1;
         }else{
             msg = "It's Draw! Scissors can't beat Scissors!";
         }
